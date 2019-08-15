fbce5e8f428b31c75425ce27759751b8ae92f75a
chuzasjota <chuzas.j@gmail.com>
2019-08-15 11:40:53 -0500

    Create blog page + change single page + link logo home

--- wp-content/themes/dixoTheme/page-blog.php
+++ wp-content/themes/dixoTheme/page-blog.php
@@ -3,38 +3,40 @@
 <div class="container">
 	<div class="row">
 		<div class="col-12">
+			<h2 class="title-lg text-center">
+				BLOG
+			</h2>
+		</div>
+		<div class="col-12">
 			<main role="main">
 				<!-- section -->
 				<section class="color-grisoscuro">
-					<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+					<?php
+						$blogs = get_posts();
+						if($blogs):
+					?>
 
 						<!-- article -->
 						<div class="container">
+							<?php foreach ($blogs as $blog):
+								$blog_img = wp_get_attachment_url( get_post_thumbnail_id($blog->ID, 'full') );
+								$blog_excerpt = get_the_excerpt($blog->ID);
+							?>
 							<div class="row align-items-center item-blog">
-								<div class="col-12 col-md-4">
-									<img src="http://localhost/dixo/wp-content/uploads/2019/08/lineadirecta.png" alt="" class="w-100">
-								</div>
+								<?php if($blog_img): ?>
+									<div class="col-12 col-md-4">
+										<img src="<?php echo $blog_img; ?>" alt="" class="w-100">
+									</div>
+								<?php endif; ?>
 								<div class="col-12 col-md-8">
-									<h2><a href="">NOTIMAGAZINE1</a></h2>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eligendi laborum maxime harum ex nihil quas saepe porro expedita, ullam eveniet voluptatem, accusantium optio officia beatae. Aliquam odio, quasi autem?
-									</p>
-									<a href="" class="btn-general">Ver más</a>
+									<h2><a href="<?php echo $blog->guid; ?>"><?php echo $blog->post_title; ?></a></h2>
+									<p><?php echo $blog_excerpt; ?></p>
+									<a href="<?php echo $blog->guid; ?>" class="btn-general">Ver más</a>
 								</div>
 							</div>
-							<div class="row align-items-center item-blog">
-								<div class="col-12">
-									<h2><a href="">NOTIMAGAZINE1</a></h2>
-									<p>
-										Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quia eligendi laborum maxime harum ex nihil quas saepe porro expedita, ullam eveniet voluptatem, accusantium optio officia beatae. Aliquam odio, quasi autem?
-									</p>
-									<a href="" class="btn-general">Ver más</a>
-								</div>
-							</div>
+							<?php endforeach; ?>
 						</div>
 						<!-- /article -->
-
-					<?php endwhile; ?>
 
 					<?php else: ?>
 
